ffd1558105de832cde35350adbbb1e1976255316
Kelvin Romero <kelvinromero@gmail.com>
2016-08-06 21:34:25 -0300

    Comentado gera_arquivo_cpu

--- Tratamento dos Dados/apaga_PING.sh
+++ Tratamento dos Dados/apaga_PING.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Descrição: Não consegui identificar
+## Como usar:
+## Resultado:
+
+# Compandos utilizados
+## Comando - Descrição
+## sed - Filtra expressões nos arquivos
+
 for i in `seq 1 30`
 do
 
