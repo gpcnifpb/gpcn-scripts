3d076d6eff05b3f1424ba771f73b7413add56ec6
Kelvin Romero <kelvinromero@gmail.com>
2016-09-16 11:42:56 -0300

    Remover /m das variáveis do usuário
    o /m só é necessário em variáveis globais.

--- Scripts/windows/configura-windows.bat
+++ Scripts/windows/configura-windows.bat
@@ -4,10 +4,10 @@
 setx path "%PATH%;D:\Hashicorp\Vagrant\bin;C:\Program Files\Oracle\virtualbox" /m
 
 :: Definir VAGRANT_HOME (Para salvar as boxes no diretorio D:\GPCN\Boxes)
-setx VAGRANT_HOME "D:\GPCN\.vagrand.d\" /m
+setx VAGRANT_HOME "D:\GPCN\.vagrand.d\" 
 
 :: Definir VBOX_USER_HOME (Para salvar as VMs no diretorio D:\GPCN)
-setx VBOX_USER_HOME "D:\GPCN\.Virtualbox\" /m
+setx VBOX_USER_HOME "D:\GPCN\.Virtualbox\" 
 
 :: Alterar machinefolder (Para salvar as VMS no diretorio D:\GPCN\VMs)
 vboxmanage setproperty machinefolder D:\GPCN\VMs
