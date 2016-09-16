3df3cff30266ddc9d456f7562b547d1c3dabc917
Kelvin Romero <kelvinromero@gmail.com>
2016-09-16 11:49:06 -0300

    Update configura-windows.bat

--- Scripts/windows/configura-windows.bat
+++ Scripts/windows/configura-windows.bat
@@ -4,7 +4,7 @@
 setx path "%PATH%;D:\Hashicorp\Vagrant\bin;C:\Program Files\Oracle\virtualbox" /m
 
 :: Definir VAGRANT_HOME (Para salvar as boxes no diretorio D:\GPCN\Boxes)
-setx VAGRANT_HOME "D:\GPCN\.vagrand.d\" 
+setx VAGRANT_HOME "D:\GPCN\.vagrant.d\" 
 
 :: Definir VBOX_USER_HOME (Para salvar as VMs no diretorio D:\GPCN)
 setx VBOX_USER_HOME "D:\GPCN\.Virtualbox\" 
